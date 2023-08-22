6727c3c7dd032c4d507a821634b880f7da401c49
Muhammad <auwalmuhd365@gmail.com>
2023-08-22 22:07:42 +0100

    basic functionality

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,8 +1,69 @@
-const computers_choice = Math.floor(Math.random() * 3) + 1
-const rock = 1
-const paper = 2
-const scissors = 3
+const computersChoiceEl = document.getElementById('computers-choice')
+const userScoreEl = document.getElementById('user-score')
+const computerScoreEl = document.getElementById('computer-score')
+const buttons = document.querySelectorAll('button')
+const roundEl = document.getElementById('round')
+const resultEl = document.getElementById('score')
 
-function something(){
-    
+const rockId = 1
+const paperId = 2
+const scissorsId = 3
+
+let userChoice;
+let computersChoice;
+let userScore = 0
+let computerScore = 0
+let round = 1
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        showWinner()
+        userChoice = e.target.id == 'rock' ? 1 : e.target.id == 'paper' ? 2 : 3
+        computersChoice = Math.floor(Math.random() * 3) + 1
+        computersChoiceEl.innerHTML = showComputersChoice()
+        updateResult()
+        roundEl.innerHTML = round++
+        userScoreEl.innerHTML = userScore
+        computerScoreEl.innerHTML = computerScore
+    })
+})
+
+
+function showComputersChoice(){
+    if(computersChoice === rockId) return `Rock`
+    if(computersChoice === paperId) return `Paper`
+    return `Scissors`
 }
+
+function updateResult(){
+    // if user picks rock and computer paper
+    if(userChoice === 1 && computersChoice === 2) computerScore++
+    // if user picks rock and computer scissors
+    if(userChoice === 1 && computersChoice === 3) userScore++
+    // if user picks paper and computer rock
+    if(userChoice === 2 && computersChoice === 1) userScore++
+    // if user picks paper and computer scissors
+    if(userChoice === 2 && computersChoice === 3) computerScore++
+    // if user picks scissors and computer rock
+    if(userChoice === 3 && computersChoice === 1) computerScore++
+    // if user picks scissors and computer paper
+    if(userChoice === 3 && computersChoice === 2) userScore++
+
+    // anything else is a draw
+    return `draw`
+}
+
+function showWinner(){
+    if(round == 5){
+        buttons.forEach(button => {
+            button.disabled = true
+        })
+        userScore > computerScore ? resultEl.innerHTML = 'You win!' :
+        userScore < computerScore ? resultEl.innerHTML = 'You lose!' :
+        'Draw'
+    }
+}
+
+// to do 
+// option to play computer or multiplayer
+// can choose how many rounds
