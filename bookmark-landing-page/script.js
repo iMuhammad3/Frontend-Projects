44003e2d8cca608d8cd1b905c68d0e6f2a61d5bf
Muhammad <auwalmuhd365@gmail.com>
2023-09-09 17:49:29 +0100

    completed all stylings

--- bookmark-landing-page/script.js
+++ bookmark-landing-page/script.js
@@ -3,3 +3,8 @@ const menuBtn = document.querySelector('#hamburger-menu')
 menuBtn.addEventListener('click', ()=>{
     
 })
+
+// menu bar works
+// navigation links work
+// add hover states
+// make accordion work
