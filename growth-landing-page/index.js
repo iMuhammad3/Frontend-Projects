d78aa8ccb4fa1365d982ac47786f0d9be1d59765
Muhammad <auwalmuhd365@gmail.com>
2023-10-22 07:02:05 +0100

    Added items to the Header component

--- growth-landing-page/index.js
+++ growth-landing-page/index.js
@@ -4,5 +4,31 @@ const root = ReactDOM.createRoot(container)
 root.render(<App />)
 
 function App(){
-    return <h1>hi</h1>
+    return (
+        <Header />
+    )
+}
+
+function Header(){
+    const navigationItems = [
+        "Features",
+        "About us",
+        "Blog",
+    ]
+    return (
+        <header className="py-4 px-10 flex justify-between">
+            <h1>Growth</h1>
+            <ul className="flex items-center gap-2">
+                {navigationItems.map(item => <Li content={item} />)}
+                <Button content="Login" bgColor="light-grey" />
+            </ul>
+        </header>
+    )
+}
+
+function Li({content}){
+    return <li className="px-3 py-1 rounded">{content}</li>
+}
+function Button({content, bgColor}){
+    return <button className={`bg-${bgColor} px-3 py-1 rounded`}>{content}</button>
 }
