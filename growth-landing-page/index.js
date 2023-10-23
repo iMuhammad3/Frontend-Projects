2558a748922a3de12649ddee860136e5533f0dbe
Muhammad <auwalmuhd365@gmail.com>
2023-10-23 18:11:35 +0100

    almost completed price section

--- growth-landing-page/index.js
+++ growth-landing-page/index.js
@@ -12,6 +12,7 @@ function App(){
             <VideoSection />
             <hr className={utilities.colors.lightGrey} />
             <TestimonialSection />
+            <PricingSection />
         </>
     )
 }
@@ -19,7 +20,8 @@ function App(){
 const utilities = {
     classNames: {
         clickables: "px-4 py-2 rounded-md cursor-pointer",
-        sections: "py-6 px-10",
+        bigSections: "p-16",
+        smallSections: "px-16 py-6",
     },
     colors: {
         lightGrey: "bg-light-grey",
@@ -44,7 +46,7 @@ function Header(){
         setIsNavShown(prev => !prev)
     }
     return (
-        <header className={`${utilities.classNames.sections} flex items-center justify-between`}>
+        <header className={`${utilities.classNames.smallSections} flex items-center justify-between py-6`}>
             <H1 content="Growth" />
             <ul className={
                     `flex flex-col gap-2 items-center absolute inset-0 bg-white
@@ -67,16 +69,17 @@ function Header(){
 
 // HERO
 function Hero(){
-    const sectionClasses = utilities.classNames.sections
+    const padding = utilities.classNames.smallSections
+    const {colors} = utilities
     function Title(){
         return (
-            <section className={sectionClasses}>
+            <section className={padding}>
                 <div className="flex flex-col w-full gap-6 text-center md:text-start md:max-w-lg">
                     <H1 content="A powerful solution to grow your startup. Fast!" size="text-4xl" />
                     <p>Organise, collaborate, and track progress seamlessly with our one-stop-shop startup growth tool.</p>
                     <div className="flex flex-col gap-2 md:flex-row">
-                        <Button content="Get Started" bgColor={utilities.colors.blue500} classes="text-white" />
-                        <Button content="Book a demo" bgColor={utilities.colors.lightGrey} />
+                        <Button content="Get Started" bgColor={colors.blue500} classes="text-white" />
+                        <Button content="Book a demo" bgColor={colors.lightGrey} />
                     </div>
                 </div>
             </section> 
@@ -84,7 +87,7 @@ function Hero(){
     }
     function Images(){
         return (
-            <section className={`${sectionClasses} md:-mt-48 grid grid-cols-2 gap-2 md:grid-cols-[auto_auto_auto] md:items-end`}>
+            <section className={`${padding} py-6 md:-mt-48 grid grid-cols-2 gap-2 md:grid-cols-[auto_auto_auto] md:items-end`}>
                 <Image src="https://growthweb.netlify.app/img/Img-1.png" classes="col-span-2 md:col-span-1" />
                 <Image src="https://growthweb.netlify.app/img/img-2.gif" />
                 <Image src="https://growthweb.netlify.app/img/Group%202647.png" classes="h-96" />
@@ -100,16 +103,18 @@ function Hero(){
 }
 
 function VideoSection(){
+    const {colors, classNames} = utilities
     return (
-        <section className={utilities.colors.dark + " text-white flex flex-col gap-4 p-16 text-center md:items-center"}>
+        <section className={`${colors.dark} ${classNames.bigSections}  text-white flex flex-col gap-4 text-center md:items-center`}>
             <H1 content="See how it works and get started in less than 2 minutes" classes="max-w-md self-center" />
             <i>TODO: add video</i>
-            <Button content="Get Started" bgColor={utilities.colors.blue500} />
+            <Button content="Get Started" bgColor={colors.blue500} />
         </section>
     )
 }
 
 function TestimonialSection(){
+    const {colors, classNames} = utilities
     const Testimonials = [
         {
             testimonial: "Our business has seen a significant increase in productivity since we started using the Growth app.",
@@ -128,7 +133,7 @@ function TestimonialSection(){
         },
     ]
     return (
-        <section className={utilities.colors.dark + " text-white flex flex-col gap-8 p-16"}>
+        <section className={`${colors.dark} ${classNames.bigSections} + " text-white flex flex-col gap-8`}>
             <H1 
             content="Don't just take our word for it, see the success stories from businesses just like yours."
             classes="md:w-1/2"
@@ -154,9 +159,68 @@ function TestimonialSection(){
 }
 
 function PricingSection(){
+    const Prices = [
+        {
+            name: "Simple",
+            price: "Free",
+            free: true,
+            benefits: [
+                "Real-time monitoring",
+                "Track key performance indicators",
+                "Schedule appointments",
+                "Organize, delegate and keep track of tasks",
+            ],
+        },
+        {
+            name: "Premium",
+            price: "$49",
+            free: false,
+            benefits: [
+                "Data-driven decisions",
+                "Data visualisation",
+                "Schedule appointments",
+                "Store and organise contact information",
+                "Teal-time communication tools for messaging and video conferencing",
+            ],
+        },
+    ]
+
+    function Card({name, price, free, benefits}){
+        const {lightGrey, dark} = utilities.colors
+        return (
+            <div className={
+                `${free ? `${lightGrey} ` : `${dark} text-white`} 
+                w-[360px] p-8 rounded-xl flex flex-col gap-4`
+                }>
+                <h2 className="text-2xl">{name}</h2>
+                <p>
+                    Keep track of your contacts, gain valuable insights, analyse live data and performance metrics.
+                </p>
+                <div className="flex items-end gap-2">
+                    <h2 className="text-2xl leading-5">{price}</h2>
+                    <small>{free ? "* No credit card required" : <sub>/month</sub>}</small>
+                </div>
+                <hr />
+                <ul>
+                    {!free && <p>Everything from the free plan plus:</p>}
+                    {benefits.map((b, i) => <Li key={i} content={b} />)}
+                </ul>
+                <Button content="Get Started" bgColor={utilities.colors.blue500} classes="text-white" />
+            </div>
+        )
+    }
+
     return (
-        <section>
-            
+        <section className={`${utilities.classNames.bigSections} flex flex-col items-center gap-6`}>
+            <div>
+                <H1 content="Pricing" classes="text-center mb-4" />
+                <p>Start free and scale while you grow. No hidden fees. Unlimited users for free.</p>
+            </div>
+            <div className="flex flex-col gap-4 md:flex-row">
+                {Prices.map(price => {
+                return <Card name={price.name} price={price.price} free={price.free} benefits={price.benefits}  />
+                })}
+            </div>
         </section>
     )
 }
